9d26d8c6da66bbc7ebe02036494bf420607a5941
Gary Siu <Gary@untypeable.com>
2015-10-08 10:39:37 +0100

    Deleted unused and confusing login.ejs

--- app/routes.js
+++ app/routes.js
@@ -7,9 +7,9 @@ module.exports = function(app, passport) {
 
 // LOGIN
 
-  app.get('/login', function(req, res) {
-    res.render('login.ejs', { message: req.flash('loginMessage') }); 
-  });
+  // app.get('/login', function(req, res) {
+  //   res.render('login.ejs', { message: req.flash('loginMessage') }); 
+  // });
 
   app.post('/login', passport.authenticate('local-login', {
     successRedirect : '/profile', // redirect to the secure profile section
