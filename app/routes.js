05d7106288c0eea62e92bb1c9ef6b516463a7a42
Gary Siu <Gary@untypeable.com>
2015-10-08 10:49:39 +0100

    More code cleanup and changed routing to go straight to availibility on login

--- app/routes.js
+++ app/routes.js
@@ -7,12 +7,8 @@ module.exports = function(app, passport) {
 
 // LOGIN
 
-  // app.get('/login', function(req, res) {
-  //   res.render('login.ejs', { message: req.flash('loginMessage') }); 
-  // });
-
   app.post('/login', passport.authenticate('local-login', {
-    successRedirect : '/profile', // redirect to the secure profile section
+    successRedirect : '/availibility', // redirect to the secure profile section
     failureRedirect : '/', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
@@ -32,10 +28,10 @@ module.exports = function(app, passport) {
 // PROFILE SECTION
 // we will want this protected so you have to be logged in to visit
 // we will use route middleware to verify this (the isLoggedIn function)
-  app.get('/profile', isLoggedIn, function(req, res) {
-    res.render('profile.ejs', {
+  app.get('/availibility', isLoggedIn, function(req, res) {
+    res.render('availibility.ejs', {
       user : req.user // get the user out of session and pass to template
-      });
+    });
   });
 
 // LOGOUT
